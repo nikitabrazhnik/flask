340bd491d2eb7834e72adc2f112dc86565ec85f5
nikitabrazhnik <nikita.brazhnik@gmail.com>
2017-06-13 18:00:43 +0200

    1

---
+++ untitled4.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+sss
+
+next()
+dfs
+
+
+sdf
+
+if __name__ == '__main__':
+    app.run()
